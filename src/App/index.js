548fb3af3167076bf3618be6f1bccafb9946333e
Bryant Wells <bryantwells@me.com>
2018-02-10 11:23:44 -0500

    add router history

--- src/App/index.js
+++ src/App/index.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route }  from 'react-router-dom'
+import { createBrowserHistory } from 'history'
 import Greeting from './Greeting'
 import Slideshow from './Slideshow'
 import './index.css'
+
+const history = createBrowserHistory({ basename: process.env.PUBLIC_URL })
 
 class App extends Component {
 
     render () {
 
         return (
-            <Router basename={process.env.PUBLIC_URL}>
+            <Router history={history}>
                 <div className="App">
-                    <Route exact path="/" component={ Greeting }/>
-                    <Route exact path="/:slug/" component={ Slideshow }/>
-                    <Route exact path="/:slug/:activeSlide" component={ Slideshow }/>
+                    <Route exact path="/" component={Greeting} />
+                    <Route exact path="/:slug/" component={Slideshow} />
+                    <Route exact path="/:slug/:activeSlide" component={Slideshow} />
                 </div>
             </Router>
         )
